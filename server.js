01dff97a564f88c8a443aec7c3833199db9b4eab
Andres Peña <luis14-14@hotmail.com>
2020-05-28 12:42:32 -0500

    Fixing bad gateway

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
